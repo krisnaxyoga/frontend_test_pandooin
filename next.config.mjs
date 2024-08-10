100d53edde699819afbcc031cadf5eaabf565e61
krisnaxyoga <yogakris.yk@gmail.com>
2024-08-10 14:36:12 +0700

    tes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+            use: {
+                loader: "url-loader",
+                options: {
+                    limit: 100000,
+                    name: "[name].[ext]",
+                    outputPath: "./",
+                    publicPath: "/",
+                },
+            },
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
+
